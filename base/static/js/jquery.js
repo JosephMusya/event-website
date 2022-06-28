8ec1fe4d7b59ea7130a73d9d3385e9d5d105e948
JosephMusya <josephmusya254@gmail.com>
2022-06-28 21:56:09 +0300

    Modified

--- base/static/js/jquery.js
+++ base/static/js/jquery.js
@@ -1,27 +1,34 @@
-$(document).ready(function() {
-    $("#delete").click(function(e) {
-        // alert("Do you want to delete?")
-        e.preventDefault();
+// $(document).ready(function() {
+//     var details = $(this).closest('.myBody').find('.details').text()
+//     // console.log(details)
+//     var details50 = details.subs(0, 50);
+//     $('.details').html(details50 + '...')
+// })
 
-        // var product_name = $(this).closest('.product-info').find('.prod-name').val();
+// $(document).ready(function() {
+//     $("#delete").click(function(e) {
+//         // alert("Do you want to delete?")
+//         e.preventDefault();
 
-        var message_id = $(this).closest('.msg').find('.message_id').val();
-        var message_user = $(this).closest('.msg').find('.message_user').val();
-        var token = $('input[name=csrfmiddlewaretoken]').val();
+//         // var product_name = $(this).closest('.product-info').find('.prod-name').val();
 
-        $.ajax({
-            method: 'POST',
-            url: 'delete-msg/',
-            data: {
-                'message_id': message_id,
-                'message_user': message_user,
-                csrfmiddlewaretoken: token,
-            },
-            success: function(response) {
-                location.reload(true);
-                //window.location.reload(true);
-            }
-        });
-    });
+//         var message_id = $(this).closest('.msg').find('.message_id').val();
+//         var message_user = $(this).closest('.msg').find('.message_user').val();
+//         var token = $('input[name=csrfmiddlewaretoken]').val();
 
-});
+//         $.ajax({
+//             method: 'POST',
+//             url: 'delete-msg/',
+//             data: {
+//                 'message_id': message_id,
+//                 'message_user': message_user,
+//                 csrfmiddlewaretoken: token,
+//             },
+//             success: function(response) {
+//                 location.reload(true);
+//                 //window.location.reload(true);
+//             }
+//         });
+//     });
+
+// });

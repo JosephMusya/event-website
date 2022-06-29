e66f74489e5e6a5feb2106a4834ed0aeecc50b25
JosephMusya <josephmusya254@gmail.com>
2022-06-29 21:16:10 +0300

    Added more urls

--- base/urls.py
+++ base/urls.py
@@ -1,18 +1,20 @@
-from django.urls import path #The url path
-from . import views #import views from the current folder to map the url
+from django.urls import path, include #The url path
+# from . import views #import views from the current folder to map the url
+from .views import *
 
 from django.conf import settings
 from django.conf.urls.static import static
 #Create the urls
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('event/<str:pk>', views.event, name='event'),
-    path('login/',views.loginUser, name='login'),
-    path('logout/',views.logoutUser, name='logout'),
-    path('register/',views.registerUser, name='register'),
-    path('delete-msg/',views.deleteMsg, name='delete-msg'),
-    path('create-event/',views.createEvent, name='create-event'),
-    path('profile/<str:pk>',views.profile, name='profile'),
+    path('', home, name='home'),    
+    path('event/<str:pk>', event, name='event'),
+    path('accounts/login/',loginUser, name='login'),
+    path('logout/',logoutUser, name='logout'),
+    path('register/',registerUser, name='register'),
+    path('delete-msg/',deleteMsg, name='delete-msg'),
+    path('create-event/',createEvent, name='create-event'),
+    path('profile/<str:pk>',profile, name='profile'),
+    path('account/<str:pk>',viewAccount, name='my-account'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

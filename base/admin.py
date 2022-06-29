dcbde6bd262c2b6c45bee216dd124e2c2c98844a
JosephMusya <josephmusya254@gmail.com>
2022-06-29 21:14:03 +0300

    Modified User model

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from django.contrib import admin
+from .models import Event,Message,Profile
+from django.contrib.auth.admin import UserAdmin
+from django.forms import Textarea
 
 # Register your models here.
-from .models import Event,Message,Profile
 admin.site.register(Event)
 admin.site.register(Message)
 admin.site.register(Profile)

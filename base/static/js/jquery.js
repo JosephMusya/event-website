237355466230448dd1fceae3d0db55e9f5e59146
JosephMusya <josephmusya254@gmail.com>
2022-06-30 23:54:12 +0300

    First Commit

--- base/static/js/jquery.js
+++ base/static/js/jquery.js
@@ -5,30 +5,58 @@
 //     $('.details').html(details50 + '...')
 // })
 
-// $(document).ready(function() {
-//     $("#delete").click(function(e) {
-//         // alert("Do you want to delete?")
-//         e.preventDefault();
+$(document).ready(function() {
+    $("#join-event").click(function(e) {
+        // alert("Do you want to delete?")
+        e.preventDefault();
 
-//         // var product_name = $(this).closest('.product-info').find('.prod-name').val();
+        // var product_name = $(this).closest('.product-info').find('.prod-name').val();
 
-//         var message_id = $(this).closest('.msg').find('.message_id').val();
-//         var message_user = $(this).closest('.msg').find('.message_user').val();
-//         var token = $('input[name=csrfmiddlewaretoken]').val();
+        var event_id = $(this).closest('#event-box').find('#event').val();
+        var user_id = $(this).closest('#event-box').find('#user').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
 
-//         $.ajax({
-//             method: 'POST',
-//             url: 'delete-msg/',
-//             data: {
-//                 'message_id': message_id,
-//                 'message_user': message_user,
-//                 csrfmiddlewaretoken: token,
-//             },
-//             success: function(response) {
-//                 location.reload(true);
-//                 //window.location.reload(true);
-//             }
-//         });
-//     });
+        $.ajax({
+            method: 'POST',
+            url: '/add-event/',
+            data: {
+                'event_id': event_id,
+                'user-id': user_id,
+                csrfmiddlewaretoken: token,
+            },
+            success: function(response) {
+                location.reload(true);
+                //window.location.reload(true);
+            }
+        });
+    });
 
-// });
+});
+
+$(document).ready(function() {
+    $("#revoke-event").click(function(e) {
+        // alert("Do you want to delete?")
+        e.preventDefault();
+
+        // var product_name = $(this).closest('.product-info').find('.prod-name').val();
+
+        var event_id = $(this).closest('#event-box').find('#event').val();
+        var user_id = $(this).closest('#event-box').find('#user').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+
+        $.ajax({
+            method: 'POST',
+            url: '/revoke-event/',
+            data: {
+                'event_id': event_id,
+                'user-id': user_id,
+                csrfmiddlewaretoken: token,
+            },
+            success: function(response) {
+                location.reload(true);
+                //window.location.reload(true);
+            }
+        });
+    });
+
+});

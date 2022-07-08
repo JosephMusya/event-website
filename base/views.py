882c1e57cbe1cb8a7206646df6014fb61e6062a3
JosephMusya <josephmusya254@gmail.com>
2022-07-08 23:54:33 +0300

    Modified views

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 
 #http response is used to display info in the browser
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 from .models import Event, Message, Attendees
 
@@ -12,27 +13,13 @@ from .forms import CustomUserForm
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
-# Create your views here.
 
-#The request parameter is the request by the user, to be handled
-# def hello(request):
-#     message = 'Hello World This is my Hello World Displayed on a browser'
-#     return HttpResponse(message)
+from django.db.models import Q
+from datetime import datetime
 
-# def home(request):
-#     users = [
-#         {'id':1,'name':'Josee','course':'Mechatronics'},
-#         {'id':1,'name':'Amalia','course':'Mechatronics'},
-#         {'id':1,'name':'Verol','course':'Electrical & electronics'},
-#         {'id':1,'name':'Salome','course':'Mechatronics'},
-
-#         ]
-#     context = {'users':users}
-#     return render(request,'base/home.html',context)
-
-def home(request):
-    events = Event.objects.all()
+def home(request):        
     users = User.objects.all()
+    available_events = []
     if request.user.is_authenticated:
         events_attending = Attendees.objects.filter(user = request.user)
         event_ids = []    
@@ -41,19 +28,31 @@ def home(request):
             event_ids.append(ids)
     else:
         event_ids = None
-    context = {'events':events,'users':users,'event_ids':event_ids}
-
-    return render(request,'base/homepage.html',context)
+        
+    if request.method == "GET":
+        q = request.GET.get('search') if request.GET.get('search') != None else ''
+        available_events = Event.objects.filter(
+            Q(topic=q)
+        )
+        if available_events:
+            context = {'events':available_events,'users':users,'event_ids':event_ids}
+            return render(request,'base/homepage.html',context)
+        else:
+            available_events = Event.objects.all()              
+            context = {'events':available_events,'users':users,'event_ids':event_ids}        
+            return render(request,'base/homepage.html',context)
 
 def event(request,pk):
     content = Event.objects.get(id=pk)
     attendees = Attendees.objects.filter(event=content)
-    events_attending = Attendees.objects.filter(user = request.user)
     event_ids = []
-    for events in events_attending:
-        ids = events.event.id
-        event_ids.append(ids)
     messages = Message.objects.filter(event_id=pk)
+    if request.user.is_authenticated:
+        events_attending = Attendees.objects.filter(user = request.user)        
+        for events in events_attending:
+            ids = events.event.id
+            event_ids.append(ids)
+    
     
     context = {'content':content,'messages':messages,'event_ids':event_ids,'attendees':attendees}
     return render(request,'base/event.html', context)
@@ -64,7 +63,7 @@ def registerUser(request):
         form = CustomUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('login')
         
     context = {'form':form}
     return render(request, 'base/register.html', context)
@@ -85,7 +84,6 @@ def logoutUser(request):
 
 @login_required(login_url='login')
 def addEvent(request):
-    print("Added successfully...")
     attendee = Attendees()
     event_id = int(request.POST.get('event_id'))
     user_id = int(request.POST.get('user-id'))
@@ -110,11 +108,14 @@ def createEvent(request):
         event.location = request.POST.get('Location')
         event.about = request.POST.get('About')
         event.date_start = request.POST.get('Date1')
+        
         event.date_end = request.POST.get('Date2')
         event.capacity = request.POST.get('capacity')
-        event.pic = request.POST.get('image')
+        event.pic = request.FILES['image']
         
         event.save()
+        
+        return redirect('/home')
         
     return render(request,'base/create_event.html')
 
@@ -168,3 +169,8 @@ def deleteMessage(request):
     if request.user == user:
         message.delete()      
     return redirect('/')
+
+def searchEvent(request):
+    events = Event.objects.filter().values_list('topic',flat=True)
+    event_list = list(events)
+    return JsonResponse (event_list, safe=False)
